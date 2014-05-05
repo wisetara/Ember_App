8619754c3a7ecf70bd5e1a0d61f4f5e27d12c8fe
wisetara <taratirzah@gmail.com>
2014-05-04 22:40:18 -0700

    nest reviews in products and products link to crafters

--- js/app.js
+++ js/app.js
@@ -4,6 +4,10 @@ App = Ember.Application.create({
 
 App.ApplicationAdapter = DS.FixtureAdapter.extend();
 
+//To use a RESTful server instead of fixtures, 
+//host the info in JSON style on a server or elsewhere, and use
+//App.ApplicationAdapter = DS.RESTAdapter.extend();
+
 App.Router.map(function() {
   this.route('credits', { path: '/thanks' });
   this.resource('products', function() {
@@ -64,9 +68,44 @@ App.Product = DS.Model.extend({
 	price: DS.attr('number'),
 	description: DS.attr('string'),
 	isOnSale: DS.attr('boolean'),
-	image: DS.attr('string')
+	image: DS.attr('string'),
+  reviews: DS.hasMany('review', {async: true}),
+  crafter: DS.belongsTo('contact', {async: true})
 });
 
+App.Contact = DS.Model.extend({
+  name: DS.attr('string'),
+  avatar: DS.attr('string'),
+  about: DS.attr('string'),
+  products: DS.hasMany('product', { async: true })
+});
+
+App.Review = DS.Model.extend({
+  text: DS.attr('string'),
+  reviewedAt: DS.attr('date'),
+  product: DS.belongsTo('product')
+});
+
+/*FIXTURES
+//In a RESTful server situation, these would be in a JSON "Products" file (perhaps)
+//And the syntax would be 
+{ "products": [
+    {
+      "id": 1,
+      "title": "Flint",
+      "price": 99,
+      "description": "Flint is...",
+      "inOnSale": true,
+      "image": "/assets/products/flint.png",
+      "reviews": [100,101]
+    }, {...}
+  ],
+  "reviews": [
+    {"id": 100, "product": 1, "text": "Started a fire in no time"}, ...
+  ]
+}
+
+  ]*/
 App.Product.FIXTURES = [
   {
     id: 1,
@@ -74,7 +113,9 @@ App.Product.FIXTURES = [
     price: 99,
     description: "Flint is...",
     isOnSale: true,
-    image: 'images/flint.png'
+    image: 'images/flint.png',
+    reviews: [100,101],
+    crafter: 1
   },
   {
     id: 2,
@@ -82,27 +123,38 @@ App.Product.FIXTURES = [
     price: 249,
     description: "Easily...",
     isOnSale: false,
-    image: 'images/kindling.png'
+    image: 'images/kindling.png',
+    reviews: [],
+    crafter: 2
   }
 ];
-
-App.Contact = DS.Model.extend({
-	name: DS.attr(),
-	avatar: DS.attr(),
-	about: DS.attr()
-});
 
 App.Contact.FIXTURES = [
   {
     id: 1,
     name: "Flint Edwards",
     avatar: 'images/contacts/flint.png',
-    about: 'Whatever'
+    about: 'Whatever',
+    products: [1]
   },
   {
     id: 2,
     name: "Kindling Smith",
     avatar: 'images/contacts/kindling.png',
-    about: 'Whatever'
+    about: 'Whatever',
+    products: [2]
   }
 ];
+
+App.Review.FIXTURES = [
+  { 
+    id: 100,
+    product: 1,
+    text: "Started a fire in no time!"
+  },
+  {
+    id: 101,
+    product: 1,
+    text: "Not the brightest flame, but warm!"
+  }
+]
